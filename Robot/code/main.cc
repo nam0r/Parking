25e542a1a8ff757df71497d2e4b784ffdaa7c353
MKRTCHIAN <mkrtchia@polytech.unice.fr>
2012-01-04 01:24:00 +0100

    main

--- Robot/code/main.cc
+++ Robot/code/main.cc
@@ -1,11 +1,34 @@
 #include "Robot.h"
 #include "AfficheurConsole.h"
 
+using namespace std;
+
 int main() {
 	Robot robot;
 	AfficheurConsole console(&robot);
 	robot.attache(&console);
+	Obstacle *o;
+	Objet *ob;
 
 	robot.figer();
 	robot.repartir();
+	robot.avancer(2, 3);
+	robot.tourner("W");
+	
+	//interdit
+	try {
+		robot.peser();
+	} catch(exception &e) {
+		cerr << e.what() << endl;
+	}
+	
+	robot.rencontrerObstacle(o);
+	
+	robot.evaluerObstacle();
+	robot.saisir(ob);
+	robot.poser();
+	robot.saisir(ob);
+	robot.peser();
+	robot.tourner("E");
+	
 }
